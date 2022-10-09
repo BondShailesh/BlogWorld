070caa906f58c1011d28cdd542223ab302d6fe75
BondShailesh <shaileshrajvicky@gmail.com>
2022-10-09 12:11:20 +0530

    frontend and backend done

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -24,7 +24,7 @@ function Homepage() {
           position="absolute"
           left={["0%", "0%", "0%"]}
           right={["0%", "0%", "0%"]}
-          top={["130px", "50px"]}
+          top={["80px", "50px"]}
         >
           <Heading fontSize={["16px", "20px", "34px"]}>
             Publish your passions, your way
